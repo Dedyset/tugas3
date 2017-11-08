5c9b930929ce840e5650f9b43576605758b5649a
Dedyset <dedy.setyyawan@gmail.com>
2017-11-08 08:08:09 +0700

    Underweight

--- 5.cpp
+++ 5.cpp
@@ -5,3 +5,13 @@ using namespace std;
 int main()
 {
 	double tinggi, berat, under, over;
+		
+		cout<<"Masukan tinggi badan [cm}: ";cin>>tinggi;
+	cout<<"Masukan berat badan [kg]: ";cin>>berat;
+	
+	under = tinggi/2.5;
+	over = tinggi/2.3;
+	if (berat < under)
+		{
+		cout<<"Underweight" ;
+	}
