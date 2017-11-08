1a7c4e01668fd3a25112792307c9aa13f3f9bf86
Dedyset <dedy.setyyawan@gmail.com>
2017-11-08 07:37:35 +0700

    inputan atau masukan nilai x dan y

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+	int x, y, z;
+	cout << "Input nilai x = ";
+	cin  >> x;
+	
+	cout << "Input nilai y = ";
+	cin  >> y;
+	
